3d30f0dd09d88d6ce57b195f26c2b6da796ebd9d
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2023-07-19 19:14:02 +0200

    Preserve session data

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer')
 const { log } = require('cozy-konnector-libs')
+const fs = require('fs')
 
 module.exports = {
   getClientTokens: async function () {
@@ -20,60 +21,74 @@ module.exports = {
     // each login requires providing an SMS-sent OTP code and there's no way
     // to get a permanent token
     log('info', 'Get token')
-    let browser = await puppeteer.launch({ headless: false })
+    let dataDir = `./data/${username}`
+    // create data dir if it doesn't exist
+    if (!fs.existsSync(dataDir)) {
+      fs.mkdirSync(dataDir)
+    }
+    let browser = await puppeteer.launch({
+      headless: false,
+      userDataDir: dataDir
+    })
     let page = await browser.newPage()
     await page.goto('https://myedenred.fr/connexion')
 
+    await page.waitForNetworkIdle()
+
     try {
-      const onetrust = '#onetrust-accept-btn-handler'
-      const onetrustBtn = await page.waitForSelector(onetrust, {
-        timeout: 1000
-      })
-      log('info', 'Cookies')
-
-      if (onetrustBtn !== null) {
-        await Promise.all([page.waitForNetworkIdle(), page.click(onetrust)])
-      }
+      await page.waitForSelector('#btn-profil-nav', { timeout: 1000 })
     } catch (e) {
-      //
-    }
+      try {
+        const onetrust = '#onetrust-accept-btn-handler'
+        const onetrustBtn = await page.waitForSelector(onetrust, {
+          timeout: 1000
+        })
+        log('info', 'Cookies')
 
-    await page.waitForSelector('input[name="Username"]')
-    log('info', 'username found')
-    const form = await page.$('form')
+        if (onetrustBtn !== null) {
+          await Promise.all([page.waitForNetworkIdle(), page.click(onetrust)])
+        }
+      } catch (e) {
+        //
+      }
 
-    await form.$('input[name="Username"]').then(el => el.type(username))
-    await form.$('input[name="Password"]').then(el => el.type(password))
-    await page.waitForTimeout(200)
-    await form.$('button[type="submit"]').then(el => el.click())
+      await page.waitForSelector('input[name="Username"]')
+      log('info', 'username found')
+      const form = await page.$('form')
 
-    await page.waitForSelector('input.verifyotp-number', { timeout: 20000 })
+      await form.$('input[name="Username"]').then(el => el.type(username))
+      await form.$('input[name="Password"]').then(el => el.type(password))
+      await page.waitForTimeout(200)
+      await form.$('button[type="submit"]').then(el => el.click())
 
-    const two_fa_form = await page.$('form')
-    if (two_fa_form) {
-      await connector.deactivateAutoSuccessfulLogin()
+      await page.waitForSelector('input.verifyotp-number', { timeout: 20000 })
 
-      /* let code;
-                        if (process.env.COZY_JOB_MANUAL_EXECUTION !== 'true') {
-                            const readline = require('readline').createInterface({
-                                input: process.stdin,
-                                output: process.stdout
-                            });
-                            code = await new Promise(resolve => readline.question('Enter 2FA code: ', resolve));
-                            readline.close();
-                        } else {
-                            await connector.waitForTwoFaCode();
-                        }
-            
-                        // code is in form 123456
-                        // fields are input class="verifyotp-number" data-index="N" with N in [0, 5]
-                        for (let i = 0; i < 6; i++) {
-                            await two_fa_form.$(`input[class="verifyotp-number"][data-index="${i}"]`).then(el => el.type(code[i]));
-                        }
-                        await two_fa_form.$('button[id="sendOtp"]').then(el => el.click());
-            
-                        await page.wai*/
-      await page.waitForSelector('#btn-profil-nav', { timeout: 0 })
+      const two_fa_form = await page.$('form')
+      if (two_fa_form) {
+        await connector.deactivateAutoSuccessfulLogin()
+
+        /* let code;
+                          if (process.env.COZY_JOB_MANUAL_EXECUTION !== 'true') {
+                              const readline = require('readline').createInterface({
+                                  input: process.stdin,
+                                  output: process.stdout
+                              });
+                              code = await new Promise(resolve => readline.question('Enter 2FA code: ', resolve));
+                              readline.close();
+                          } else {
+                              await connector.waitForTwoFaCode();
+                          }
+              
+                          // code is in form 123456
+                          // fields are input class="verifyotp-number" data-index="N" with N in [0, 5]
+                          for (let i = 0; i < 6; i++) {
+                              await two_fa_form.$(`input[class="verifyotp-number"][data-index="${i}"]`).then(el => el.type(code[i]));
+                          }
+                          await two_fa_form.$('button[id="sendOtp"]').then(el => el.click());
+              
+                          await page.wai*/
+        await page.waitForSelector('#btn-profil-nav', { timeout: 0 })
+      }
     }
 
     log('info', 'Logged in')
